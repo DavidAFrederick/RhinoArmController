ce499d616adb48d622944aef58aaebc9ab5fcb96
David Frederick <DavidAFrederick.va.usa.earth@gmail.com>
2023-12-03 20:17:16 -0500

    Updating Menu

--- main.py
+++ main.py
@@ -12,6 +12,7 @@ IF1bus = I2C(addressInterface1)
 #==========================================================================================
 def displayBasicMenu():
     os.system('clear')
+    print (" ")
     print ("Interface A [ Gripper Pinch  ] - [Not started] - [45] - [12345]") 
     print ("Interface B [ Wrist Rotation ] - [In Process ] - [00] - [12345]")
     print ("Interface C [ Wrist Flex     ] - [Complete   ] - [00] - [12345]")
@@ -26,6 +27,216 @@ def displayBasicMenu():
     print ("Set Angle - 11-xx, 21-xx, 31-xx")
     print ("Set Count - 12-xxxx, 22-xxxx, 33-xxxx")
     print ("Exit - 0")
+
+#==========================================================================================
+def displaycoloredMenu():
+
+    # IFA_status = "InProcess"
+    # IFA_status = "Complete"
+    IFA_status = "Unknown"
+    IFA_angle = 90                #  2 characters width
+    IFA_count = 11111             #  6 characters width
+
+
+    # IFB_status = "InProcess"
+    # IFB_status = "Complete"
+    IFB_status = "Unknown"
+    IFB_angle = 90                #  2 characters width
+    IFB_count = 11111             #  6 characters width
+
+    # IFC_status = "InProcess"
+    # IFC_status = "Complete"
+    IFC_status = "Unknown"
+    IFC_angle = 90                #  2 characters width
+    IFC_count = 11111             #  6 characters width
+
+    # IFD_status = "InProcess"
+    # IFD_status = "Complete"
+    IFD_status = "Unknown"
+    IFD_angle = 90                #  2 characters width
+    IFD_count = 11111             #  6 characters width
+
+    # IFE_status = "InProcess"
+    # IFE_status = "Complete"
+    IFE_status = "Unknown"
+    IFE_angle = 90                #  2 characters width
+    IFE_count = 11111             #  6 characters width
+
+    # IFF_status = "InProcess"
+    # IFF_status = "Complete"
+    IFF_status = "Unknown"
+    IFF_angle = 90                #  2 characters width
+    IFF_count = 11111             #  6 characters width
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    IFX_status_display_In_Process = "In Process "
+    IFX_status_display_Complete = " Complete  "
+    IFX_status_display_Unknown = " Unknown   "
+
+    os.system('clear')
+    print (" ")
+    print ("Interface A [ Gripper Pinch  ] - [", end="")    
+
+    if (IFA_status == "Unknown"):   
+        print (Fore.RED, end="")
+        IFA_status_display = IFX_status_display_Unknown
+    
+    if (IFA_status == "InProcess"): 
+        print (Fore.CYAN, end="")
+        IFA_status_display = IFX_status_display_In_Process
+
+    if (IFA_status == "Complete"):  
+        print (Fore.GREEN, end="")
+        IFA_status_display = IFX_status_display_Complete
+
+    print (IFA_status_display, end="")
+    print (Fore.WHITE, end="")
+
+    print("] - [",        end="")
+    print (IFA_angle,     end="")
+    print("] - [",        end="")
+    print (IFA_count,     end="")
+    print("]")
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    print ("Interface B [ Wrist Rotation ] - [", end="")    
+
+    if (IFA_status == "Unknown"):   
+        print (Fore.RED, end="")
+        IFB_status_display = IFX_status_display_Unknown
+    
+    if (IFB_status == "InProcess"): 
+        print (Fore.CYAN, end="")
+        IFB_status_display = IFX_status_display_In_Process
+
+    if (IFB_status == "Complete"):  
+        print (Fore.GREEN, end="")
+        IFB_status_display = IFX_status_display_Complete
+
+    print (IFB_status_display, end="")
+    print (Fore.WHITE, end="")
+
+    print("] - [",        end="")
+    print (IFB_angle,     end="")
+    print("] - [",        end="")
+    print (IFB_count,     end="")
+    print("]")
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    print ("Interface C [ Wrist Flex     ] - [", end="")    
+
+    if (IFC_status == "Unknown"):   
+        print (Fore.RED, end="")
+        IFC_status_display = IFX_status_display_Unknown
+    
+    if (IFC_status == "InProcess"): 
+        print (Fore.CYAN, end="")
+        IFC_status_display = IFX_status_display_In_Process
+
+    if (IFC_status == "Complete"):  
+        print (Fore.GREEN, end="")
+        IFC_status_display = IFX_status_display_Complete
+
+    print (IFC_status_display, end="")
+    print (Fore.WHITE, end="")
+
+    print("] - [",        end="")
+    print (IFC_angle,     end="")
+    print("] - [",        end="")
+    print (IFC_count,     end="")
+    print("]")
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    print ("Interface D [ Elbow          ] - [", end="")    
+
+    if (IFD_status == "Unknown"):   
+        print (Fore.RED, end="")
+        IFD_status_display = IFX_status_display_Unknown
+    
+    if (IFD_status == "InProcess"): 
+        print (Fore.CYAN, end="")
+        IFD_status_display = IFX_status_display_In_Process
+
+    if (IFD_status == "Complete"):  
+        print (Fore.GREEN, end="")
+        IFD_status_display = IFX_status_display_Complete
+
+    print (IFD_status_display, end="")
+    print (Fore.WHITE, end="")
+
+    print("] - [",        end="")
+    print (IFD_angle,     end="")
+    print("] - [",        end="")
+    print (IFD_count,     end="")
+    print("]")
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    print ("Interface E [ Shoulder       ] - [", end="")    
+
+    if (IFE_status == "Unknown"):   
+        print (Fore.RED, end="")
+        IFE_status_display = IFX_status_display_Unknown
+    
+    if (IFE_status == "InProcess"): 
+        print (Fore.CYAN, end="")
+        IFE_status_display = IFX_status_display_In_Process
+
+    if (IFE_status == "Complete"):  
+        print (Fore.GREEN, end="")
+        IFE_status_display = IFX_status_display_Complete
+
+    print (IFE_status_display, end="")
+    print (Fore.WHITE, end="")
+
+    print("] - [",        end="")
+    print (IFE_angle,     end="")
+    print("] - [",        end="")
+    print (IFE_count,     end="")
+    print("]")
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+    print ("Interface F [ Waist          ] - [", end="")    
+
+    if (IFF_status == "Unknown"):   
+        print (Fore.RED, end="")
+        IFF_status_display = IFX_status_display_Unknown
+    
+    if (IFF_status == "InProcess"): 
+        print (Fore.CYAN, end="")
+        IFF_status_display = IFX_status_display_In_Process
+
+    if (IFF_status == "Complete"):  
+        print (Fore.GREEN, end="")
+        IFF_status_display = IFX_status_display_Complete
+
+    print (IFF_status_display, end="")
+    print (Fore.WHITE, end="")
+
+    print("] - [",        end="")
+    print (IFF_angle,     end="")
+    print("] - [",        end="")
+    print (IFF_count,     end="")
+    print("]")
+
+    # - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - 
+
+    print ("Interface B [ Wrist Rotation ] - [In Process ] - [00] - [12345]")
+    print ("Interface C [ Wrist Flex     ] - [Complete   ] - [00] - [12345]")
+    print ("Interface D [ Elbow          ] - [UnKnown    ] - [00] - [12345]")
+    print ("Interface E [ Shoulder       ] - [UnKnown    ] - [00] - [12345]")
+    print ("Interface F [ Waist          ] - [UnKnown    ] - [00] - [12345]")
+    print (" ")
+    print (F"{Style.BRIGHT}Commands:")
+    print (F"{Style.NORMAL}Stop all - 1")
+    print ("Home all - 2")
+    print ("Home One - 10, 20, 30")
+    print ("Set Angle - 11-xx, 21-xx, 31-xx")
+    print ("Set Count - 12-xxxx, 22-xxxx, 33-xxxx")
+    print ("Exit - 0")
+
+
+#==========================================================================================
 
 #==========================================================================================
 def stopAllMotors():
@@ -132,8 +343,9 @@ def main():
 
     done = False
     while not done:
-        displayBasicMenu()
-        userCommand = input(">>")
+        # displayBasicMenu()
+        displaycoloredMenu()
+        userCommand = input(">>>>")
         if (userCommand == "0") or (userCommand == "q"):
             done = True
 
